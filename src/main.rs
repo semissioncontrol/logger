ccacc98fbdca4e0b1864719bb9836c3d49dddcef
Conni <31339874+ConniTheKiwi@users.noreply.github.com>
2020-11-05 20:58:42 +0000

    Added time to elapse command
    -t <time ms> 
    will run only for this set amount of time

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,20 @@
 // Written by: Bobbbay Bobbayan
 
 use std::{thread, time, fs, env};
-
+use std::time::{SystemTime, UNIX_EPOCH};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    
     let mut time = 10; // Set time in ms
     
     // If args are larger than 1 then parse them 
     if(args.len() > 1) {
-      if(args[1] == "-t") {
+      if(args[1] == "-e") {
         time = args[2].parse::<u64>().unwrap();
       }
     }
@@ -40,6 +45,20 @@ fn main() {
     // we can have an infinite loop that will read these
     // for files.
     loop {
+
+        if(args.len() > 1) {
+          if(args[1] == "-t") {
+            let start2 = SystemTime::now();
+            let since_the_epoch2 = start2
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards"); 
+            if(since_the_epoch2.as_millis() - since_the_epoch.as_millis() > args[2].parse::<u128>().unwrap()) {
+              break;
+            }
+          }
+        }
+
+
         // file_num stores the amount of status files
         // detected. This number should hopefully be as
         // little as possible, because status files are
@@ -57,7 +76,7 @@ fn main() {
         }
         // Finally, print out the status files and wait 1ms
 
-        if(file_num >= 0) {
+        if(file_num > 0) {
           println!("{:?}", err_files);
         }
         thread::sleep(time::Duration::from_millis(time));
